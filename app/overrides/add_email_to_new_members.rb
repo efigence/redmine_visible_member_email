a9942f83323572638ffceed4745d20a79a7a1706
Jacek Grzybowski <jgrzybowski@artegence.com>
2014-02-05 15:08:50 +0100

    cleanup

--- app/overrides/add_email_to_new_members.rb
+++ app/overrides/add_email_to_new_members.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "projects/settings/_members",
                      :name => "add_email_to_new_members",
                      :replace => "div#principals_for_new_member",
-                     :text => "<div id='principals_for_new_member'><%= render_principals_for_new_members_with_emails(@project) %></div>")
+                     :text => "<div id='principals_for_new_member'><%= render_principals_for_new_members(@project) %></div>")
